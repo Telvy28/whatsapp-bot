07f614ae1f0d3ae54faba2b7944818e55914be9a
Telvy Pizarro Loayza <tpizarrol@outlook.com>
2025-12-07 09:59:36 -0500

    Update whatsappservices.py

--- whatsappservices.py
+++ whatsappservices.py
@@ -6,7 +6,7 @@ def SendMessageWhatsapp(data):
     try:
         token = os.getenv("WHATSAPP_TOKEN")
         phone_id = os.getenv("PHONE_NUMBER_ID")
-        api_url = f"https://graph.facebook.com/v22.0/{phone_id}/messages"
+        api_url = f"https://graph.facebook.com/v21.0/{phone_id}/messages"
         headers = {"content-type": "application/json", "authorization": "Bearer " + token}
         response = requests.post(api_url, data=json.dumps(data), headers=headers)
         
